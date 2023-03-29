ebdb82deeeefd361f8d7ee6679dadfec5c135b89
김상두 <puki4416@gmail.com>
2023-03-29 19:37:05 +0900

    feat : Header 컴포넌트 제작

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import * as styles from "./Header.module.css"
+
+const Header = ({ children, path }) => {
+  return (
+    <header className={styles.container}>
+      <div className={styles.wrap}>
+        <Link className={styles.logo} to="/">
+          {children}
+        </Link>
+        <div className={styles.linkWrap}>
+          <Link
+            className={`${styles.link} ${path === "/" ? styles.select : ""}`}
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className={`${styles.link} ${
+              path === "/about" ? styles.select : ""
+            }`}
+            to="about"
+          >
+            Abouts
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
