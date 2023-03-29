01b55308e6e41fc665904db781a8c2a4511ce843
김상두 <puki4416@gmail.com>
2023-03-29 19:44:13 +0900

    feat : 404 페이지 구현

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,7 +10,7 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>주소를 잘못 입력하셨습니다.</p>
     </Layout>
   )
 }
