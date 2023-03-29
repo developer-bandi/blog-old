8f4165987e34c4223760516697ba4d6b06bdff7c
김상두 <puki4416@gmail.com>
2023-03-29 19:43:32 +0900

    feat : blog-post 페이지 제작

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,63 +1,134 @@
 import * as React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Header from "../components/Header/Header"
+import Footer from "../components/Footer/Footer"
+import Utterances from "../components/Utterances/Utterances"
+import { useEffect } from "react"
+import { useRef } from "react"
 
 const BlogPostTemplate = ({
-  data: { previous, next, site, markdownRemark: post },
+  data: { site, markdownRemark: post },
   location,
 }) => {
   const siteTitle = site.siteMetadata?.title || `Title`
+  const bodyRef = useRef(null)
+  const tocRef = useRef(null)
+  const raf = useRef()
+  console.log(post.tableOfContents)
 
+  useEffect(() => {
+    const headerTag = ["H1", "H2", "H3", "H4", "H5", "H6"]
+    const bodyEl = bodyRef.current.children
+    const headerNames = []
+    const headerPosition = []
+
+    const tocElement = tocRef.current.querySelectorAll("a")
+    const tocAnchorElements = {}
+
+    for (let i = 0; i < bodyEl.length; i++) {
+      if (headerTag.includes(bodyEl[i].tagName)) {
+        headerNames.push(bodyEl[i].id)
+        headerPosition.push(
+          bodyEl[i].getBoundingClientRect().top + window.pageYOffset
+        )
+      }
+    }
+
+    for (let i = 0; i < tocElement.length; i++) {
+      const el = tocElement[i]
+      const href = el.href.split("/")[el.href.split("/").length - 1]
+      tocAnchorElements[href] = el
+    }
+    console.log(headerNames)
+    console.log(headerPosition)
+    console.log(tocAnchorElements)
+
+    const changeStyle = name => {
+      const el = tocAnchorElements[`#${encodeURI(name)}`]
+      Object.values(tocAnchorElements).forEach(element => {
+        element.style.color = "#9e9e9e"
+        element.style.fontWeight = "400"
+      })
+      if (el !== null) {
+        el.style.color = "black"
+        el.style.fontWeight = "600"
+      }
+    }
+
+    const scrollEventHandler = () => {
+      const position = document.documentElement.scrollTop
+      raf.current = window.requestAnimationFrame(() => {
+        if (position < headerPosition[0]) {
+          changeStyle(headerNames[0])
+        }
+        for (let i = headerPosition.length - 1; i > 0; i--) {
+          if (headerPosition[i] < position) {
+            changeStyle(headerNames[i])
+            break
+          }
+        }
+      })
+    }
+    window.addEventListener("scroll", scrollEventHandler)
+    scrollEventHandler()
+    return () => {
+      window.removeEventListener("scroll", scrollEventHandler)
+      if (raf.current !== undefined) {
+        window.cancelAnimationFrame(raf)
+      }
+    }
+  }, [])
   return (
-    <Layout location={location} title={siteTitle}>
+    <div>
+      <Header path={location.pathname}>{siteTitle}</Header>
       <article
-        className="blog-post"
+        style={{
+          padding: "100px 20px 0 20px",
+          maxWidth: "700px",
+          margin: "auto",
+        }}
         itemScope
         itemType="http://schema.org/Article"
       >
         <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
+          <h1 itemProp="headline" style={{ marginBottom: "10px" }}>
+            {post.frontmatter.title}
+          </h1>
+          <p style={{ marginBottom: "30px" }}>{post.frontmatter.date}</p>
         </header>
+        <img
+          src={`${window.location.protocol}//${
+            window.location.host
+          }/thumbnail${location.pathname.substring(
+            0,
+            location.pathname.length - 1
+          )}.jpg`}
+          alt="thumbnail"
+          style={{
+            width: "100%",
+            height: "300px",
+            borderRadius: "20px",
+            marginBottom: "30px",
+            objectFit: "cover",
+          }}
+        />
         <section
           dangerouslySetInnerHTML={{ __html: post.html }}
           itemProp="articleBody"
+          ref={bodyRef}
+        />
+        <section
+          dangerouslySetInnerHTML={{ __html: post.tableOfContents }}
+          className="toc"
+          ref={tocRef}
         />
         <hr />
-        <footer>
-          <Bio />
-        </footer>
+        <Utterances />
       </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </Layout>
+      <Footer />
+    </div>
   )
 }
 
@@ -73,11 +144,7 @@ export const Head = ({ data: { markdownRemark: post } }) => {
 export default BlogPostTemplate
 
 export const pageQuery = graphql`
-  query BlogPostBySlug(
-    $id: String!
-    $previousPostId: String
-    $nextPostId: String
-  ) {
+  query BlogPostBySlug($id: String!) {
     site {
       siteMetadata {
         title
@@ -87,26 +154,11 @@ export const pageQuery = graphql`
       id
       excerpt(pruneLength: 160)
       html
+      tableOfContents
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "YYYY. MM. DD")
         description
-      }
-    }
-    previous: markdownRemark(id: { eq: $previousPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-    next: markdownRemark(id: { eq: $nextPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
       }
     }
   }
