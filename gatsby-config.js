6dd4c99239a4504c0d12b16706074ff75ad83c73
김상두 <puki4416@gmail.com>
2023-03-30 16:40:55 +0900

    feat : google 방문자 수 분석 tool 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,17 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-6JV8B8FXNL", // 설정 Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
