5d1742d0a85470426415a62e070fdd85a70cd3ae
김상두 <puki4416@gmail.com>
2023-03-30 16:22:20 +0900

    fix : home title 한글로 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -92,7 +92,7 @@ export default BlogIndex
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="All posts" />
+export const Head = () => <Seo title="전체 포스트" />
 
 export const pageQuery = graphql`
   {
