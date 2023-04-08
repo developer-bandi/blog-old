f54cdcc7daa3379e48cc50e179ee02c95013f4cb
김상두 <puki4416@gmail.com>
2023-04-09 02:06:22 +0900

    feat : code highlight line number 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,16 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: true,
+              noInlineHighlight: false,
+            },
+          },
           {
             resolve: "gatsby-remark-autolink-headers",
             options: {
